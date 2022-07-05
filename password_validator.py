1b6f6d6c6ab027b20f31d64a4edc176c95ae07a6
Michael <michael24kolet@gmail.com>
2022-07-05 15:20:35 +0300

    Change in user instructions

--- password_validator.py
+++ password_validator.py
@@ -10,6 +10,12 @@ dict_of_checks = {"min_ten": False,
 
 
 def IsValid(string: str) -> bool:
+    """
+    check validation of string by the rules
+    :param string: password
+    :return: true if the password is valid else false
+    """
+
     global dict_of_checks
 
     for ch in string:
@@ -35,23 +41,21 @@ def IsValid(string: str) -> bool:
     return True
 
 
-
-
 def main():
     password = sys.argv[1]
     text_color = not Fore
 
-    dict_of_mssages = {"alphabet": "Letter must be entered in the password !!!",
-                       "digit": "Number must be entered in the password !!!",
-                       "min_ten": "Password too short !!!",
-                       "small": "A small letter must be in the password",
-                       "capital": "A capital letter must be in the password"
-                       }
+    dict_of_messages = {"alphabet": "Letter must be entered in the password !!!",
+                        "digit": "Number must be entered in the password !!!",
+                        "min_ten": "Password too short! At least 10 characters must be entered",
+                        "small": "A small letter must be in the password",
+                        "capital": "A capital letter must be in the password"
+                        }
+
     if not IsValid(password):
         for item in dict_of_checks:
             if not dict_of_checks[item]:
-                raise TypeError(Fore.RED + dict_of_mssages[item])
-                break
+                raise TypeError(Fore.RED + dict_of_messages[item])
 
     print(Fore.GREEN + "passed the validation")
 
